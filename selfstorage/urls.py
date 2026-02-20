042ebb84563aed7515c350c5b89d90114645f604
runcken <runcken@gmail.com>
2026-02-20 20:28:13 +0300

    add db

--- selfstorage/urls.py
+++ selfstorage/urls.py
@@ -8,7 +8,7 @@ from storage import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('', include('users.urls')),
     path('storage/', include('storage.urls')),
 ]
